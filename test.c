571c2a68b6875fea7dd082dadf24c18901057cdb
Ksenob1te <63819958+Ksenob1te@users.noreply.github.com>
2022-12-02 13:38:21 +0300

    Add files via upload

---
+++ test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "stdlib.h"
+
+int checker(char el) {
+    char* types = ",.:;!?";
+    for (int i = 0; types[i] != '\0' ; ++i) {
+        if (el == types[i]) return 1;
+    }
+    return 0;
+}
+
+char* removeWhitespaces(char* str) {
+    char *newstr = calloc(201, sizeof(char));
+    int counter = 0;
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; ++i) {
+        if (!(str[i] == ' ') || (!checker(str[i + 1])))
+            newstr[counter++] = str[i];
+    }
+    return newstr;
+}
+
+int main() {
+    char str[201];
+    printf("Type in string: \n");
+    fgets(str, 200, stdin);
+    char *result = removeWhitespaces(str);
+    printf("%s", result);
+    free(result);
+}
